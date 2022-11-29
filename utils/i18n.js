e675603581223424900712a92e3af69ddb32314f
Baobao-bao <55513182+Baobao-bao@users.noreply.github.com>
2022-11-29 17:16:03 +0800

    start dealing with multiple video currentTime indicator

--- utils/i18n.js
+++ utils/i18n.js
@@ -1,29 +1,30 @@
-import i18n from "i18next";
+import i18n from 'i18next';
 // import LanguageDetector from "i18next-browser-languagedetector";
-import { initReactI18next } from "react-i18next";
+import { initReactI18next } from 'react-i18next';
 // use(LanguageDetector)
 i18n.use(initReactI18next).init({
-    lng: "en",
-    resources: {
-        en: {
-            translations: {
-                "Room name": "房间名",
-                "Make it public": "公开房间",
-            },
-        },
+  lng: 'en',
+  resources: {
+    en: {
+      translations: {
+        'Room name': '房间名',
+        'Make it public': '公开房间',
+        Search: '搜索',
+      },
     },
-    fallbackLng: "en",
-    debug: true,
+  },
+  fallbackLng: 'en',
+  debug: true,
 
-    // have a common namespace used around the full app
-    ns: ["translations"],
-    defaultNS: "translations",
+  // have a common namespace used around the full app
+  ns: ['translations'],
+  defaultNS: 'translations',
 
-    keySeparator: false, // we use content as keys
+  keySeparator: false, // we use content as keys
 
-    interpolation: {
-        escapeValue: false,
-    },
+  interpolation: {
+    escapeValue: false,
+  },
 });
 
 export default i18n;
